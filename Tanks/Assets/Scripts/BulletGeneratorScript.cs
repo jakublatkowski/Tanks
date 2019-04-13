11e32756e7eb40d77296c7ecfac29a05bba3d3de
Wierzkac <wierzkac@gmail.com>
2019-04-13 11:47:53 +0200

    GeneratorBulletów

---
+++ Tanks/Assets/Scripts/BulletGeneratorScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGeneratorScript : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obj, transform.position, Quaternion.identity);
+    }
+}
