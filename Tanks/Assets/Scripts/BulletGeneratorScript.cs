5f8268785d80b573f05e05adc767de3eaae72b0e
Wierzkac <wierzkac@gmail.com>
2019-04-20 22:18:04 +0200

    To strzela :O

--- Tanks/Assets/Scripts/BulletGeneratorScript.cs
+++ Tanks/Assets/Scripts/BulletGeneratorScript.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class BulletGeneratorScript : MonoBehaviour
 {
     public GameObject obj;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject bulletGenerator;
+    
+    public void Spawn()
     {
-        Instantiate(obj, transform.position, Quaternion.identity);
+        GameObject tmp = Instantiate(obj, bulletGenerator.transform.position, bulletGenerator.transform.rotation);
+        Rigidbody bullet = tmp.GetComponent<Rigidbody>();
+        bullet.velocity = bulletGenerator.transform.forward * 50;
+        Rigidbody tank = bulletGenerator.GetComponentInParent<Rigidbody>();
+        tank.velocity = bulletGenerator.transform.forward * -5;
+
+
+        Debug.Log("Spawn");
     }
 }
