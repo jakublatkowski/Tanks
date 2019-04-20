11d568beb69bf23c68be251f5f52d4bb262c4acc
jakublatkowski <jakublatkowski21@gmail.com>
2019-04-20 17:51:55 +0200

    Poprawka sterowania
    Dodanie powrotu handlera do położenia domyślnego

---
+++ Tanks/Assets/Scripts/ResetBarValuesScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ResetBarValuesScript : MonoBehaviour, IPointerExitHandler
+{
+
+    public Scrollbar scrollbar;
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        scrollbar.value = 0.5f;
+    }
+}
