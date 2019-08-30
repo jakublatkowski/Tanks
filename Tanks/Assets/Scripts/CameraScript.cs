df8563cdf6bbfdd93b362cdadd4de1e12cd74436
jakublatkowski <jakublatkowski21@gmail.com>
2019-08-30 21:59:18 +0200

    Integrations

--- Tanks/Assets/Scripts/CameraScript.cs
+++ Tanks/Assets/Scripts/CameraScript.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public Transform tank;
+    public TankController tankController;
     Vector3 offset;
 
     // Update is called once per frame
     private void Start()
     {
-        transform.position = new Vector3(tank.position.x, tank.position.y + 2, tank.position.z - 3);
-        transform.rotation = new Quaternion(tank.rotation.x, tank.rotation.y, tank.rotation.z, tank.rotation.w);
-        offset = tank.transform.position - transform.position;
-
+        SetUpCameraPosition(tankController.transform);
     }
+
     void LateUpdate()
     {
-        float angle = tank.transform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, angle, 0);
-        transform.position = tank.transform.position - (rotation * offset);
-        Vector3 targetPosition = new Vector3(tank.position.x, transform.position.y, tank.position.z);
+        var tankControllerTransform = tankController.transform;
+
+        SetUpCameraPosition(tankControllerTransform);
+
+        var angle = tankControllerTransform.transform.eulerAngles.y;
+
+        var rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = tankControllerTransform.transform.position - (rotation * offset);
+
+        var targetPosition = new Vector3
+        {
+            x = tankControllerTransform.position.x,
+            y = transform.position.y,
+            z = tankControllerTransform.position.z
+        };
+
         transform.LookAt(targetPosition);
+    }
+
+    private void SetUpCameraPosition(Transform tankControllerTransform)
+    {
+        transform.position = new Vector3
+        {
+            x = tankControllerTransform.position.x,
+            y = tankControllerTransform.position.y + tankController.cameraDistance * tankController.cameraPositionYRatio,
+            z = tankControllerTransform.position.z - tankController.cameraDistance *  tankController.cameraPositionZRatio
+        };
+
+        offset = tankControllerTransform.transform.position - transform.position;
     }
 }
