56ec04f30e9fda9e01dfde312627a78893626f73
Fosz <dreamkakuzu@gmail.com>
2019-09-19 23:09:17 +0200

    kolorowanie
    - merge ścianek areny FFA z resztą terenu
    - kolorowanie areny drużynowej w zależności od podanych kolorów na wejście
    - drobne zmiany

--- Tanks/Assets/Scripts/StartGameScript.cs
+++ Tanks/Assets/Scripts/StartGameScript.cs
@@ -11,6 +11,7 @@ public class StartGameScript : MonoBehaviour
     void Start()
     {
         CreatePlayer();
+        SetArenaTeamColors(new Color(0.5f, 0.0f, 0.5f), new Color(0.0f, 0.5f, 0.5f));
     }
 
     // Update is called once per frame
@@ -19,7 +20,56 @@ public class StartGameScript : MonoBehaviour
         var spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
         var index = Random.Range(0, spawnPoints.Length);
         var spawnPoint = spawnPoints[index];
-
+        var test = Path.Combine("Prefabs", "WOJSKOOOO");
         tank = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Tank"), spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+        Material[] keks = tank.GetComponent<Renderer>().materials;
+        foreach(Material kek in keks)
+        {
+            kek.SetColor("_Color", new Color(1.0f, 0.0f, 0.0f));
+        }
+    }
+
+    private void SetArenaTeamColors(Color redTeamColor, Color blueTeamColor)
+    {
+        Dictionary<string, int> intensityDict = new Dictionary<string, int>
+        {
+            { "tint", 5 },
+            { "light", 4 },
+            { "medium", 3 },
+            { "hard", 2 },
+            { "extreme", 1 },
+            { "wall", 0 }
+        };
+
+        GameObject terrain = GameObject.Find("TDM_Battlefield");
+        Material[] materials = terrain.GetComponent<Renderer>().materials;
+
+        int maxIntensity = intensityDict.Count - 1;
+        foreach (Material material in materials)
+        {
+            foreach (string key in intensityDict.Keys)
+            {
+                if (material.name.Contains(key))
+                {
+                    float multiplier = 1.0f / maxIntensity * intensityDict[key];
+                    // przyjęta konwencja domyślnych kolorów red i blue
+                    if (material.name.Contains("Red"))
+                    {
+                        material.SetColor("_Color", new Color(
+                            redTeamColor.r + (1 - redTeamColor.r) * multiplier,
+                            redTeamColor.g + (1 - redTeamColor.g) * multiplier,
+                            redTeamColor.b + (1 - redTeamColor.b) * multiplier));
+                    }
+                    else
+                    {
+                        material.SetColor("_Color", new Color(
+                            blueTeamColor.r + (1 - blueTeamColor.r) * multiplier,
+                            blueTeamColor.g + (1 - blueTeamColor.g) * multiplier,
+                            blueTeamColor.b + (1 - blueTeamColor.b) * multiplier));
+                    }
+                }
+            }
+        }
     }
 }
