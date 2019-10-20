ba90c86934f0076abda516fbefc2c8b020077691
Fosz <dreamkakuzu@gmail.com>
2019-10-21 00:10:44 +0200

    skrypt kolorowania +ukrycie widoczności spawn pointów

--- Tanks/Assets/Scripts/StartGameScript.cs
+++ Tanks/Assets/Scripts/StartGameScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using System.Linq;
 
 public class StartGameScript : MonoBehaviour
 {
@@ -19,7 +20,20 @@ public class StartGameScript : MonoBehaviour
         var spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
         var index = Random.Range(0, spawnPoints.Length);
         var spawnPoint = spawnPoints[index];
-
         tank = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "SuperTank"), spawnPoint.transform.position, spawnPoint.transform.rotation);
+        SetTankColor(tank, new Color(0.25f, 0.0f, 0.25f));
+    }
+
+    private void SetTankColor(GameObject tank, Color color)
+    {
+        Component[] tank_parts = tank.GetComponentsInChildren<Component>(true);
+        foreach (var mat in from Component part in tank_parts
+                            let mats = part.GetComponent<Renderer>().materials
+                            from Material mat in mats
+                            where mat.name.Contains("Primary")
+                            select mat)
+        {
+            mat.SetColor("_Color", color);
+        }
     }
 }
